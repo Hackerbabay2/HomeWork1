a84a3681a5590024af3ca5e42db671d7bb7e4b25
hacker babay <75236889+Hackerbabay2@users.noreply.github.com>
2022-01-04 15:49:08 +0800

    aaa
    aaa

--- HomeWork/Assets/Scripts/Signaling.cs
+++ HomeWork/Assets/Scripts/Signaling.cs
@@ -5,49 +5,54 @@ using UnityEngine.Events;
 
 public class Signaling : MonoBehaviour
 {
-    [SerializeField] private UnityEvent _reached;
-    [SerializeField] Animator _otherObjectAnimator;
-    
-    public bool IsPlaying;
-    
+    [SerializeField] private UnityEvent _signaling;
     private AudioSource _audioSource;
-    private float delay;
-
+    private bool _isSignaling;
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
-        IsPlaying = false;
-        delay = 0.01f;
     }
 
-    private IEnumerator StartSignaling()
+    public void StartSignaling()
     {
-        _otherObjectAnimator.SetBool("Alerm", true);
+        _isSignaling = true;
+        _audioSource.volume = 0;
+        _audioSource.Play();
+        StartCoroutine(Play());
+    }
 
-        while (IsPlaying)
-        {
-            for (float i = 0; i < 1; i += delay)
-            {
-                _audioSource.volume += Mathf.MoveTowards(0,1,delay);
-                yield return null;
-            }
-
-            for (float i = 1; i > 0; i -= delay)
-            {
-                _audioSource.volume -= Mathf.MoveTowards(0,1,delay);
-                yield return null;
-            }
-        }
+    public void StopSignaling()
+    {
+        _isSignaling = false;
+        _audioSource.volume = 0;
+        _audioSource.Stop();
+        StopCoroutine(Play());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent<Player>(out Player player))
         {
-            _reached.Invoke();
-            IsPlaying = true;
-            StartCoroutine(StartSignaling());
+            _signaling.Invoke();
+        }
+    }
+
+    private IEnumerator Play()
+    {
+        while (_isSignaling)
+        {
+            for (float i = 0; i < 1; i += 0.01f )
+            {
+                _audioSource.volume += Mathf.MoveTowards(0,1,0.01f);
+                yield return null;
+            }
+
+            for (float i = 1; i > 0; i -= 0.01f)
+            {
+                _audioSource.volume -= Mathf.MoveTowards(0,1,0.01f);
+                yield return null;
+            }
         }
     }
 }
---
+++ HomeWork/Assets/Scripts/SwitchAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private const string Alerm = "Alerm";
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        _animator.SetBool(Alerm, true);
+    }
+
+    public void StopAnimation()
+    {
+        _animator.SetBool(Alerm,false);
+    }
+}
